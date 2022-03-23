d8a9af82b2d9a52ace70352818fb55bad3b13919
Antonio Cirillo <dev.cirillo@gmail.com>
2022-03-23 17:43:27 +0100

    update library

--- blocking-communication/library/mycollective.c
+++ blocking-communication/library/mycollective.c
@@ -5,17 +5,28 @@
 #define MY_TAG 99
 
 
-void MPI_Gathering_send(Item* value, MPI_Datatype datatype, MPI_Comm comm, int rank) {
+void MPI_Gathering_send(Item* value, MPI_Datatype datatype, MPI_Comm comm) {
+    
+    int rank;
+    MPI_Comm_rank(comm, &rank);
+    
     printf("[Processo #%d] - Invio dato...\n", rank);
     MPI_Send(value, 1, datatype, 0, MY_TAG, comm);
     printf("[Processo #%d] - Valore inviato: ", rank);
     out_item(value);
     printf("\n");
+
 }
 
-void MPI_Gathering_recv(Item buffer[], MPI_Datatype datatype, int np, 
-        MPI_Comm comm, MPI_Status* status, int rank) {
+void MPI_Gathering_recv(Item buffer[], MPI_Datatype datatype, 
+        MPI_Comm comm, MPI_Status* status) {
     
+    int np;
+    int rank;
+
+    MPI_Comm_size(comm, &np);
+    MPI_Comm_rank(comm, &rank);
+
     for(int i = 1; i < np; i++)
         MPI_Recv(&buffer[i - 1], 1, datatype, i, MY_TAG, comm, status);
     
@@ -27,3 +38,43 @@ void MPI_Gathering_recv(Item buffer[], MPI_Datatype datatype, int np,
 
 }
 
+void MPI_Broadcast_send(Item* value, int size, MPI_Datatype datatype, MPI_Comm comm) {
+
+    int np;
+    int rank;
+
+    MPI_Comm_size(comm, &np);
+    MPI_Comm_rank(comm, &rank);
+
+    printf("[Processo #%d] - Comunico il numero di valori...", rank);
+    for(int i = 1; i < np; i++) {
+        MPI_Send(&size, 1, MPI_INT, i, MY_TAG, comm);
+    }
+    printf("[Processo #%d] -  Numero di valori comunicato!", rank);
+
+    printf("[Processo #%d] - Comunico i valori...", rank);
+    for(int i = 1; i < np; i++) {
+        MPI_Send(value, size, datatype, i, MY_TAG, comm);
+    }
+    printf("[Processo #%d] - Valori comunicati!", rank);
+
+}
+
+void MPI_Broadcast_revc(Item buffer[], MPI_Datatype datatype,
+        MPI_Comm comm, MPI_Status* status) {
+
+    int rank;
+    MPI_Comm_rank(comm, &rank);
+
+    int size;
+    MPI_Recv(&size, 1, MPI_INT, 0, MY_TAG, comm, &status);
+
+    MPI_Recv(buffer, size, datatype, 0, MY_TAG, comm, &status);
+    printf("[Processo #%d] - Valori ricevuti: [", rank);
+    for(int i = 0; i < size - 1; i++) {
+        printf("%d ", buffer[i]);
+    }
+    printf("%d]\n", buffer[size - 1]);
+
+
+}
--- blocking-communication/library/mycollective.h
+++ blocking-communication/library/mycollective.h
@@ -1,6 +1,11 @@
 #include "item.h"
 
-void MPI_Gathering_send(Item* value, MPI_Datatype datatype, MPI_Comm comm, int rank);
+void MPI_Gathering_send(Item* value, MPI_Datatype datatype, MPI_Comm comm);
 
-void MPI_Gathering_recv(Item buffer[], MPI_Datatype datatype, int np, 
-    MPI_Comm comm, MPI_Status* status, int rank);
+void MPI_Gathering_recv(Item buffer[], MPI_Datatype datatype,
+        MPI_Comm comm, MPI_Status* status);
+
+void MPI_Broadcast_send(Item* value, int size, MPI_Datatype datatype, MPI_Comm comm);
+
+void MPI_Broadcast_revc(Item buffer[], MPI_Datatype datatype,
+        MPI_Comm comm, MPI_Status* status);
